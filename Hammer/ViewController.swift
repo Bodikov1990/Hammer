c3a60fabfd3a5faec98f8e3e5f4d430dae7cd91d
Bodikov Kuat <kuat.bodikov@gmail.com>
2023-04-03 18:29:53 +0500

    Initial Commit

---
+++ Hammer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hammer
+//
+//  Created by Kuat Bodikov on 03.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
